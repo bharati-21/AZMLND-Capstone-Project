8b3d5228bd66f78ae3abe80f2db13005b1984d3f
Bharati <bharatisharada@gmail.com>
2021-02-02 13:27:45 +0000

    Edited score.py

--- files/score.py
+++ files/score.py
@@ -2,20 +2,21 @@
 import json
 import numpy as np
 import os
-from sklearn.externals import joblib
+import pickle
+# import sklearn.external.joblib as extjoblib
+import joblib
+# from sklearn.linear_model import LogisticRegression
+
+from azureml.core.model import Model
 
 
 def init():
     global model
-    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), './outputs/model.joblib')
+    model_path = Model.get_model_path('./outputs/model.joblib')
     model = joblib.load(model_path)
 
 def run(data):
-    try:
-        data = np.array(json.loads(raw_data)['data'])
-        result = model.predict(data)
-        # You can return any data type, as long as it is JSON serializable.
-        return result.tolist()
-    except Exception as e:
-        error = str(e)
-        return error
+    data = np.array(json.loads(raw_data)['data'])
+    result = model.predict(data)
+    return json.dumps(result.tolist())
+    
